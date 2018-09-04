995658899740827f6dce03027f573ac045750b27
Anton <romanenko-anton@ro.ru>
2018-09-04 19:24:18 +0400

    a

---
+++ reloader.js
@@ -0,0 +1,12 @@
+document.body.onload = functian(){
+setTimeout(functian(){
+var preloader = document.getElementById('page-preloader');
+if (!preloader.classList.contains('done')) 
+{
+
+preloader.classList.add('done');
+}, 1000);
+
+}
+  
+}
